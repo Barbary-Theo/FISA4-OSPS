d8337b6d0f9c95f452361a1b5602bbf127c62cea
maaart1 <maaart1.thib@gmail.com>
2022-10-11 16:48:51 +0200

    (automatic commit by a script)

--- pairesTubesNommesProcess.py
+++ pairesTubesNommesProcess.py
@@ -1,6 +1,5 @@
-import os
-import pairetubesnommesprocessalancerenpremier as first
-import pairetubesnommesprocessalancerensecond as second
+import os, sys
+from multiprocessing import shared_memory
 
 
 def mkfifo(path):
@@ -11,7 +10,31 @@ def mkfifo(path):
         pass
 
 
-def prog():
+def shm():
+    shm_segment1 = shared_memory.SharedMemory(name='012345', create=True, size=10)
+    print('Nom du segment mémoire partagée :', shm_segment1.name)
+    print('Taille du segment mémoire partagée en octets via premier accès :', len(shm_segment1.buf))
+    shm_segment1.buf[:10] = bytearray([74, 73, 72, 71, 70, 69, 68, 67, 66, 65])
+
+    shm_segment2 = shared_memory.SharedMemory(shm_segment1.name)
+    print('Taille du segment mémoire partagée en octets via second accès :', len(shm_segment2.buf[:10]))
+    print('Contenu du segment mémoire partagée en octets via second accès :', bytes(shm_segment2.buf[:10]))
+
+    newpid = os.fork()
+    if newpid < 0:
+        print("fork() impossible")
+        os.abort()
+    if newpid == 0:
+        os.execlp("ipcs", "ipcs", "-m")
+    else:
+        os.wait()
+
+    shm_segment2.close()
+    shm_segment1.close()
+    shm_segment1.unlink()
+
+
+def main():
     to_red = lambda text: "\033[382{}{}{}m{} \033[382255255255m".format(255, 0, 0, text)
 
     pathtube1 = "/tmp/tubenommeprincipalsecond"
@@ -21,88 +44,60 @@ def prog():
     mkfifo(pathtube2)
 
     try:
-        if pid := os.fork() < 0:
+        pid = os.fork()
+        if pid < 0:
             print("It's not possible to fork() !")
         elif pid == 0:
-            print('Création des tubes...')
+            print('Overture du tube1 en écriture...')
+            fifo1 = open(pathtube1, "w")
 
-            # Ci-dessous "0o" instroduit un nombre en octal
+            print('Overture du tube2 en lecture...')
+            fifo2 = open(pathtube2, "r")
 
-            for i in range(3):
-                print('Overture du tube1 en écriture...')
+            print('Processus principal prêt pour échanger des messages...')
+            print('Écriture dans le tube1...')
 
-                # Ouverture en écriture est bloquante si pas de lecteur
+            fifo1.write("PING\n")
 
-                fifo1 = open(pathtube1, "w")
+            print('Fermeture du tube1...')
+            fifo1.close()
 
-                print('Overture du tube2 en lecture...')
+            print('Processus principal en attente de réception de messages...')
+            for line in fifo2:
+                print("Message recu : " + line)
 
-                # Ouverture en lecture est bloquante si pas de rédacteur
-
-                fifo2 = open(pathtube2, "r")
-
-                print('Processus principal prêt pour échanger des messages...')
-
-                print('Écriture dans le tube1...')
-
-                fifo1.write("Message 1 du processus principal !\n")
-                fifo1.write("Message 2 du processus principal !\n")
-
-                print('Fermeture du tube1...')
-
-                fifo1.close()
-
-                print('Processus principal en attente de réception de messages...')
-
-                for line in fifo2:
-                    print("Message recu : " + line)
-
-                print('Fermeture du tube2...')
-
-                fifo2.close()
+            print('Fermeture du tube2...')
+            fifo2.close()
 
             print('Destruction des tubes...')
-
             os.unlink(pathtube1)
             os.unlink(pathtube2)
         else:
+            print('Overture du tube1 en lecture...')
+            fifo1 = open(pathtube1, "r")
 
-            for i in range(3):
-                print('Overture du tube1 en lecture...')
+            print('Overture du tube2 en écriture...')
+            fifo2 = open(pathtube2, "w")
 
-                # Ouverture en lecture est bloquante si pas de rédacteur
+            print('Processus secondaire prêt pour échanger des messages...')
 
-                fifo1 = open(pathtube1, "r")
+            print('Processus secondaire en attente de réception de messages...')
 
-                print('Overture du tube2 en écriture...')
+            for line in fifo1:
+                print("Message recu : " + line)
 
-                # Ouverture en écriture est bloquante si pas de lecteur
+            print('Fermeture du tube1...')
+            fifo1.close()
 
-                fifo2 = open(pathtube2, "w")
+            print('Écriture dans le tube2...')
+            fifo2.write("PONG\n")
 
-                print('Processus secondaire prêt pour échanger des messages...')
-
-                print('Processus secondaire en attente de réception de messages...')
-
-                for line in fifo1:
-                    print("Message recu : " + line)
-
-                print('Fermeture du tube1...')
-
-                fifo1.close()
-
-                print('Écriture dans le tube2...')
-
-                fifo2.write("Message 1 du processus secondaire !\n")
-                fifo2.write("Message 2 du processus secondaire !\n")
-
-                print('Fermeture du tube2...')
-
-                fifo2.close()
+            print('Fermeture du tube2...')
+            fifo2.close()
 
     except Exception as e:
         print(to_red(e.__str__()))
 
 
 if __name__ == "__main__":
-    prog()
+    main()
