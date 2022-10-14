96627634ea01949716d147f1c3085cc19a1c6463
maaart1 <maaart1.thib@gmail.com>
2022-10-14 08:45:37 +0200

    (automatic commit by a script)

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os, sys
 from multiprocessing import shared_memory
+from shm import shm
 
 
 def mkfifo(path):
@@ -9,31 +10,6 @@ def mkfifo(path):
     except:
         pass
 
-
-def shm():
-    shm_segment1 = shared_memory.SharedMemory(name='012345', create=True, size=10)
-    print('Nom du segment mémoire partagée :', shm_segment1.name)
-    print('Taille du segment mémoire partagée en octets via premier accès :', len(shm_segment1.buf))
-    shm_segment1.buf[:10] = bytearray([74, 73, 72, 71, 70, 69, 68, 67, 66, 65])
-
-    shm_segment2 = shared_memory.SharedMemory(shm_segment1.name)
-    print('Taille du segment mémoire partagée en octets via second accès :', len(shm_segment2.buf[:10]))
-    print('Contenu du segment mémoire partagée en octets via second accès :', bytes(shm_segment2.buf[:10]))
-
-    newpid = os.fork()
-    if newpid < 0:
-        print("fork() impossible")
-        os.abort()
-    if newpid == 0:
-        os.execlp("ipcs", "ipcs", "-m")
-    else:
-        os.wait()
-
-    shm_segment2.close()
-    shm_segment1.close()
-    shm_segment1.unlink()
-
-
 def main():
     to_red = lambda text: "\033[382{}{}{}m{} \033[382255255255m".format(255, 0, 0, text)
 
@@ -42,6 +18,12 @@ def main():
 
     mkfifo(pathtube1)
     mkfifo(pathtube2)
+
+    try:
+        print("Création du segment mémoire partagée")
+        shm()
+    except Exception as e:
+        print(to_red(e.__str__()))
 
     try:
         pid = os.fork()
