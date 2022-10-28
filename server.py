90bfeb1b8bc328f959362ea65c5bfb483679a30d
Barbary-Theo <theo.barbary25@gmail.com>
2022-10-28 10:27:57 +0200

    (automatic commit by a script) text to write in shared memory can be input

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os, sys
+import random
 from multiprocessing import shared_memory
 from time import sleep
 from random import randint
@@ -10,17 +11,17 @@ from shm import shm
 
 
 def mkfifo(path):
-    # NE PAS FAIRE, C'EST DÉGEU, MAIS ON A PAS LE CHOIX DU COUP ON LE FAIT
     try:
         os.mkfifo(path, 0o0600)
     except:
         pass
 
 
-
 def main_server(pathtube1, pathtube2):
     shm_segment1 = shared_memory.SharedMemory("shm_osps")
     rand = randint
+
+    random_text = ["mot", "sus", "BEANSS", "Python", "cheval"]
 
     while True:
 
@@ -28,10 +29,12 @@ def main_server(pathtube1, pathtube2):
             fifo1 = open(pathtube1, "w")
             fifo2 = open(pathtube2, "r")
 
-            shm_segment1.buf[:7] = bytearray([71, 70, 69, 68, 67, 66, 65])
+            text_to_write = input("Server 1 have to write -> ")
+
+            shm_segment1.buf[:len(text_to_write)] = bytearray([ord(value) for value in text_to_write])
 
             print("Serveur 1 a écrit")
-            fifo1.write("I wrote\n")
+            fifo1.write(str(len(text_to_write)) + "\n")
             fifo1.flush()
 
             print("Serveur 1 à lu : ",  fifo2.readline().replace("\n", ""))
@@ -53,8 +56,10 @@ def secondary_server(pathtube1, pathtube2):
             fifo1 = open(pathtube1, "r")
             fifo2 = open(pathtube2, "w")
 
-            print("Serveur 2 à lu : ",  fifo1.readline().replace("\n", ""))
-            print('Contenu du segment mémoire partagée en octets via second accès :', bytes(shm_segment2.buf[:7]))
+            length = fifo1.readline().replace("\n", "")
+
+            print("Serveur 2 à lu la taille : ", length)
+            print('Contenu du segment mémoire partagée en octets via second accès :', bytes(shm_segment2.buf[:int(length)]))
 
             sleep(randint(0, 5))
 
