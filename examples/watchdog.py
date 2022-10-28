d321e884f85fc78736c49e8e352c0bebba716c5d
Barbary-Theo <theo.barbary25@gmail.com>
2022-10-28 19:21:14 +0200

    (automatic commit by a script) some test

--- examples/watchdog.py
+++ examples/watchdog.py
@@ -3,8 +3,10 @@ import socket
 
 def prog():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("www.python.org", 80))
+    s.connect(("127.0.0.1", 9001))
+
+    s.send("s".encode())
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     prog()
